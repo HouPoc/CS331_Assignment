7b57e1cb44de3081e4b8c8f34b770feb958fd401
HouPoc <hou199488@gmail.com>
2017-04-16 09:47:09 +0800

    BFS done

--- Assignment_1/main.py
+++ Assignment_1/main.py
@@ -1,12 +1,22 @@
-import refer
+from refer import *
 
 
 def main():
     #
-    input_file = str(argv [1])
-    goal_file = str(argv[2])
-    mode = str(argv[3])
-    output_file = str(argv[4])
+    #input_file = str(argv [1])
+    #goal_file = str(argv[2])
+    #mode = str(argv[3])
+    #output_file = str(argv[4])
     #
     #Initial States
-    initial_state = read_source(input_file)
+    #initial_state = read_source(input_file)
+    explored = []
+    initial = state_node([0,0,0],[3,3,1])
+    goal = state_node([3,3,1],[0,0,0])
+    #print (goal_test(initial,goal))
+    result = BFS(initial, goal, explored)
+    print (path(result))
+    #print (result)
+
+if __name__ == '__main__':
+    main()
--- Assignment_1/refer.py
+++ Assignment_1/refer.py
@@ -1,119 +1,82 @@
 import sys
 
-
 class state_node ():
     
-    def __init__(self, left = None, right = None, parent = None, child = None):
+    def __init__(self, left = None, right = None, parent = None):
         self.left_bank = left
         self.right_bank = right
         self.parent = parent
-        self.child = child
+
         
 def goal_test(current, goal):
-    if current.left_bank == goal.left_bank and current.right_bank == goal.left_bank:
-        return True
-    else:
-        return False        
+    for i in range(3):
+        if current.left_bank[i] != goal.left_bank[i] or current.right_bank[i] != goal.right_bank[i]:
+            return False
+    return True
+
         
-def child_node(current,frontier):
+def check_balance(action):
+    if action.left_bank[0] >= 0 and action.left_bank[1] >= 0 and action.right_bank[0] >= 0 and action.right_bank[1] >= 0:
+       if (action.left_bank[0] >= action.left_bank[1] or action.left_bank[0]==0) and (action.right_bank[0] >= action.right_bank[1] or action.right_bank[0]==0):
+            return True
+    return False
+
+
+def child_node(current):
+    children = []
     m_l = current.left_bank[0]
     c_l = current.left_bank[1]
     b_l = current.left_bank[2]
-    m_r = current.left_bank[0]
-    c_r = current.left_bank[1]
-    b_r = current.left_bank[2]
-    # Move from left to right
-    if b_l == 1:
-    # Put one missionary in the boat
-        if m_l >= 1 and (m_l-1) > c_l and (m_r+1) > c_r:
-            m_l -= 1
-            m_r += 1
-            b_l = 0
-            b_r = 1
-            new_child = state_node ([m_l,c_l,b_l], [m_r,c_r,b_r], current)
-            frontier.append(new_child)        
-    # Put two missionaries in the boat
-        if m_l >= 2 and (m_l-2) > c_l and (m_r+2) > c_r:
-            m_l -= 2
-            m_r += 2
-            b_l = 0
-            b_r = 1
-            new_child = state_node ([m_l,c_l,b_l], [m_r,c_r,b_r], current)
-            frontier.append(new_child)      
-    # Put one cannibal in the boat
-        if c_l >= 1 and m_l > (c_l-1) and m_r > (c_r+1):
-            c_l -= 1
-            c_r += 1
-            b_l = 0
-            b_r = 1
-            new_child = state_node ([m_l,c_l,b_l], [m_r,c_r,b_r], current)
-            frontier.append(new_child) 
-            
-    # Put one cannibal and one missionary in the boat
-        if m_l >= 1 and c_l >= 1 and (m_l-1) > (c_l - 1) and (m_r+1) > (c_r+1):
-            c_l -= 1
-            m_l -= 1
-            m_r += 1
-            c_r += 1
-            b_l = 0
-            b_r = 1
-            new_child = state_node ([m_l,c_l,b_l], [m_r,c_r,b_r], current)
-            frontier.append(new_child)
-        
-    # Put two cannibals in the boat
-        if c_l >= 2 and m_l > (c_l-2) and m_r > (c_r+2):
-            c_l -= 2
-            c_r += 2
-            b_l = 0
-            b_r = 1
-            new_child = state_node ([m_l,c_l,b_l], [m_r,c_r,b_r], current)
-    # Move from right to left:
-    else:
-    # Put one missionary in the boat
-        if m_r >= 1 and (m_r-1) > c_r and (m_l+1) > c_l:
-            m_l += 1
-            m_r -= 1
-            b_l = 1
-            b_r = 0
-            new_child = state_node ([m_l,c_l,b_l], [m_r,c_r,b_r], current)
-            frontier.append(new_child)        
-    # Put two missionaries in the boat
-        if m_r >= 2 and (m_r-2) > c_r and (m_l+2) > c_l:
-            m_l += 2
-            m_r -= 2
-            b_l = 1
-            b_r = 0
-            new_child = state_node ([m_l,c_l,b_l], [m_r,c_r,b_r], current)
-            frontier.append(new_child)      
-    # Put one cannibal in the boat
-        if c_r >= 1 and m_r > (c_r-1) and m_l > (c_l+1):
-            c_l += 1
-            c_r -= 1
-            b_l = 1
-            b_r = 0
-            new_child = state_node ([m_l,c_l,b_l], [m_r,c_r,b_r], current)
-            frontier.append(new_child) 
-            
-    # Put one cannibal and one missionary in the boat
-        if m_r >= 1 and c_r >= 1 and (m_r-1) > (c_r-1) and (m_l+1) > (c_l+1):
-            c_l += 1
-            m_l += 1
-            m_r -= 1
-            c_r -= 1
-            b_l = 1
-            b_r = 0
-            new_child = state_node([m_l,c_l,b_l], [m_r,c_r,b_r], current)
-            frontier.append(new_child)
-        
-    # Put two cannibals in the boat
-        if c_r >= 2 and m_r > (c_r-2) and m_l > (c_l+2):
-            c_l += 2
-            c_r -= 2
-            b_l = 1
-            b_r = 0
-            new_child = state_node([m_l,c_l,b_l], [m_r,c_r,b_r], current)
-            frontier.append(new_child)
+    m_r = current.right_bank[0]
+    c_r = current.right_bank[1]
+    b_r = current.right_bank[2]
 
+    if b_l == 1:
+        # Put one missionary in the boat
+        new_child = state_node ([m_l-1,c_l,b_l-1], [m_r,c_r+1,b_r+1], current)
+        if check_balance(new_child):
+            children.append(new_child)  
+        # Put two missionaries in the boat
+        new_child = state_node ([m_l-2,c_l,b_l-1], [m_r+2,c_r,b_r+1], current)
+        if check_balance(new_child):
+            children.append(new_child) 
+        # Put one cannibal in the boat
+        new_child = state_node ([m_l,c_l-1,b_l-1], [m_r,c_r+1,b_r+1], current)
+        if check_balance(new_child):
+            children.append(new_child) 
+        # Put one cannibal and one missionary in the boat
+        new_child = state_node ([m_l-1,c_l-1,b_l-1], [m_r+1,c_r+1,b_r+1], current)
+        if check_balance(new_child):
+            children.append(new_child) 
+        # Put two cannibals in the boat
+        new_child = state_node ([m_l,c_l-2,b_l-1], [m_r,c_r+2,b_r+1], current)
+        if check_balance(new_child):
+            children.append(new_child) 
+    else:
+        # Put one missionary in the boat
+        new_child = state_node([m_l+1,c_l,b_l+1], [m_r-1,c_r,b_r-1], current)
+        if check_balance(new_child):
+            children.append(new_child) 
+        # Put two missionaries in the boat
+        new_child = state_node ([m_l+2,c_l,b_l+1], [m_r-2,c_r,b_r-1], current)
+        if check_balance(new_child):
+            children.append(new_child) 
+        # Put one cannibal in the boat
+        new_child = state_node ([m_l,c_l+1,b_l+1], [m_r,c_r-1,b_r-1], current)
+        if check_balance(new_child):
+            children.append(new_child) 
+        # Put one cannibal and one missionary in the boat
+        new_child = state_node([m_l+1,c_l+1,b_l+1], [m_r-1,c_r-1,b_r-1], current)
+        if check_balance(new_child):
+            children.append(new_child) 
+        # Put two cannibals in the boat
+        new_child = state_node([m_l,c_l+2,b_l+1], [m_r,c_r-2,b_r-1], current)
+        if check_balance(new_child):
+            children.append(new_child)
+
+    return children
+
+            
 def read_source (file):
     f = open(file,'r')
     state = []
@@ -122,6 +85,36 @@ def read_source (file):
             state.append(map(int, line))
     return state_node(state[0],state[1])
     
+
+def is_not_in (target, collection):
+    for element in collection:
+        if goal_test (target, element):
+            return False
+    return True
+   
+
+def path (terminate_state):
+    node = terminate_state
+    route = []
+    while node is not None:
+        route.insert(0,(node.left_bank, node.right_bank))
+        node = node.parent
+    return route
+   
+def BFS(initial, goal, explored):
+    frontier = [initial]
+    while len(frontier) != 0:
+        current = frontier.pop(0)
+        explored.append(current)
+        child_nodes = child_node(current)
+        for child in child_nodes:
+            if is_not_in(child, frontier) or is_not_in(child, explored):
+                if goal_test(child,goal):
+                    return child
+                frontier.append(child)
+        #index +=1
+    return False
+               
     
     
     
@@ -131,6 +124,7 @@ def read_source (file):
     
     
     
+   
     
     
     
